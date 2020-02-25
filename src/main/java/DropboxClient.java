d8798686cd37058dd493e0436782de0025df3f24
JAZWii <jazwii2216@gmail.com>
2020-02-25 10:19:45 +0300

    first commit

---
+++ src/main/java/DropboxClient.java
@@ -0,0 +1,45 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class DropboxClient {
+    private static final String ACCESS_TOKEN = "R4Lp_bVT52AAAAAAAAAAfvfSgBWmA2EjIM83OChH19jcmknApCod7bRaBj6kQYEE";
+
+    public static void main(String args[]) throws DbxException, IOException {
+        // Create Dropbox client
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        // Get current account info
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+
+        // Get files and folder metadata from Dropbox root directory
+        ListFolderResult result = client.files().listFolder("");
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                System.out.println(metadata.getPathLower());
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("C:\\Users\\Restec2019\\Desktop\\test.txt")) {
+            FileMetadata metadata = client.files().uploadBuilder("/test.txt")
+                    .uploadAndFinish(in);
+        }
+    }
+}
