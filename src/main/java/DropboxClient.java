17e63294f97afec9c8865ab3ec52c75b5134b629
JAZWii <jazwii2216@gmail.com>
2020-02-28 19:32:49 +0300

    Service.java has been completed

--- src/main/java/DropboxClient.java
+++ src/main/java/DropboxClient.java
@@ -6,17 +6,17 @@ public class DropboxClient {
 
     public static void main(String[] args) throws DbxException, IOException {
         Service service = new Service();
-        service.getMetadata(args);
         
         if(args.length > 0){
-            //Service service = new Service();
-            
             if(args[0].equals("auth"))
                 service.getAccessToken(args);
             if(args[0].equals("info"))
                 service.getCurrentAccount(args);
             if(args[0].equals("list"))
-                service.getListFolder(args);
+                if(args[2].contains("."))
+                    service.getListFolder(args);
+                else
+                    service.getMetadata(args);
         }
         
     }
